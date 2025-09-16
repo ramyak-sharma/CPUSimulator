1184ca85bdde8425780dc50892e1290116c0b11c
ramyak-sharma <ramyaksharma1@gmail.com>
2025-09-17 02:35:25 +0530

    removed build folder

--- build/CMakeFiles/CPUSim.dir/src/components/gates/wire.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/CPUSim.dir/src/components/gates/wire.cpp.o: \
-  /Users/ramyaksharma/Desktop/CPU/src/components/gates/wire.cpp
--- build/vendor/raylib/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ramyaksharma/Desktop/CPU/vendor/raylib/src
-# Build directory: /Users/ramyaksharma/Desktop/CPU/build/vendor/raylib/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
